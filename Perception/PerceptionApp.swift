4ce8eb0ef47cbd3696bf046c525e34b12389c793
AungMinKhant <khant_aung_min01@student.smc.edu>
2026-02-28 18:20:23 -0800

    Add files via upload

---
+++ Perception/PerceptionApp.swift
@@ -0,0 +1,17 @@
+//
+//  PerceptionApp.swift
+//  Perception
+//
+//  Created by Jonathan Aung on 2/28/26.
+//
+
+import SwiftUI
+
+@main
+struct PerceptionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
